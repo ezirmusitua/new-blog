a8976401527f7fc43daf655f3d37aae78d087f51
ezirmusitua <jferroal@gmail.com>
2017-03-12 00:13:24 +0800

    Try to dockrize

--- start.py
+++ start.py
@@ -78,4 +78,4 @@ if __name__ == '__main__':
     # init mongo config file
     createMongoConfigFile()
     # run docker compose
-    subprocess.call(['docker-compose', 'up'])
+    subprocess.call(['sudo', 'docker-compose', 'up'])
