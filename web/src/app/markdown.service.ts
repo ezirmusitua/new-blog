49280fff81d726545eb6be926befdc3f1babaa82
ezirmusitua <jferroal@gmail.com>
2017-03-24 08:14:13 +0800

    update Article content view in progress

--- web/src/app/markdown.service.ts
+++ web/src/app/markdown.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser'
+
 import * as marked from 'marked';
 import * as highlight from 'highlight.js';
 
@@ -12,23 +14,42 @@ interface IMarkdownConfig {
 @Injectable()
 export class MarkdownService {
   private md: MarkedStatic;
+  private converted: SafeHtml | string;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
+    console.log(123123);
     this.md = marked.setOptions({
       highlight: (code, lang) => {
         return highlight.highlightAuto(code, [lang]).value;
       }
     });
+    console.log(234234);
   }
 
-  setConfig(config: IMarkdownConfig) {
+  public setConfig(config: IMarkdownConfig) {
     this.md = marked.setOptions(config);
   }
 
-  convert(markdown: string): string {
+  public toHtml(markdown: string) {
     if (!markdown) {
-      return '';
+      this.converted = '';
+    } else {
+      this.converted = this.md.parse(markdown);
+      console.log(this.md.parse(markdown) as string)
     }
-    return this.md.parse(markdown);
+    return this;
+  }
+
+  public sanitize() {
+    this.converted = this.sanitizer.bypassSecurityTrustHtml(this.converted as string);
+    return this;
+  }
+
+  public end() {
+    return this.converted;
+  }
+
+  public convert(markdown: string) {
+    return this.toHtml(markdown).sanitize().end() as SafeHtml;
   }
 }
